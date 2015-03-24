ebee8dc55e915125d86534d044a2625cab7d0192
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-03-24 16:52:18 +0100

    Bug 1168344: Handle Android versions in makefile
    New features in more-recent Android versions add complexity to the
    source code. This patch adds support for different features to the
    make file.

--- src/Android.mk
+++ src/Android.mk
@@ -1,18 +1,31 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= bt-av-io.c \
-                  bt-core-io.c \
-                  bt-io.c \
-                  bt-hf-io.c \
-                  bt-pdubuf.c \
-                  bt-proto.c \
-                  bt-rc-io.c \
-                  bt-sock-io.c \
-                  core.c \
-                  core-io.c \
-                  main.c \
-                  service.c
+
+bluetoothd_SRC_FILES_17 := bt-av-io.c \
+                           bt-core-io.c \
+                           bt-hf-io.c \
+                           bt-sock-io.c
+bluetoothd_SRC_FILES_18 := $(bluetoothd_SRC_FILES_17) \
+                           bt-rc-io.c
+bluetoothd_SRC_FILES_19 := $(bluetoothd_SRC_FILES_18)
+bluetoothd_SRC_FILES_20 := $(bluetoothd_SRC_FILES_19)
+bluetoothd_SRC_FILES_21 := $(bluetoothd_SRC_FILES_20)
+bluetoothd_SRC_FILES_22 := $(bluetoothd_SRC_FILES_21)
+bluetoothd_SRC_FILES    := $(bluetoothd_SRC_FILES_$(PLATFORM_SDK_VERSION)) \
+                           bt-io.c \
+                           bt-proto.c \
+                           bt-pdubuf.c \
+                           core.c \
+                           core-io.c \
+                           main.c \
+                           service.c
+
+ifeq ($(strip $(bluetoothd_SRC_FILES_$(PLATFORM_SDK_VERSION))),)
+$(error "Please set $$bluetoothd_SRC_FILES_$(PLATFORM_SDK_VERSION) in bluetoothd's makefile.")
+endif
+
+LOCAL_SRC_FILES := $(bluetoothd_SRC_FILES)
 LOCAL_C_INCLUDES := system/libfdio/include
 LOCAL_CFLAGS := -DANDROID_VERSION=$(PLATFORM_SDK_VERSION) -Wall -Werror
 LOCAL_SHARED_LIBRARIES := libfdio \
